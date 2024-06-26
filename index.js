9825f001974165bf665f03d1ed781d917454e87a
Carlos Danyel <carlosdanyelsilva27@gmail.com>
2024-06-26 15:24:09 -0300

    resolve

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const DadosDePrompt = prompt("Insira o Primeiro Valor a ser somado");
 const DadosDePromptSegundoValor = prompt("Insira o Segundo Valor");
 
-// Soma
-// const result = Number(DadosDePrompt) + Number(DadosDePromptSegundoValor);
-// alert(result);
+const resultSoma = Number(DadosDePrompt) + Number(DadosDePromptSegundoValor);
+const resultSub = Number(DadosDePrompt) - Number(DadosDePromptSegundoValor);
+alert(`a Soma é ${resultSoma}`);
+alert(`a Sub é ${resultSub}`);
 
-// Subtração
-// const result = Number(DadosDePrompt) - Number(DadosDePromptSegundoValor);
-// alert(result);
+if (DadosDePrompt === DadosDePromptSegundoValor) {
+  alert("Numeros Identicos");
+} else {
+  const result = Number(DadosDePrompt) - Number(DadosDePromptSegundoValor);
+  alert("Numeros nao identicos");
+  alert(result);
+}
 
-// Identicos
-// if (DadosDePrompt === DadosDePromptSegundoValor) {
-//   alert("Numeros Identicos");
-// } else {
-//   const result = Number(DadosDePrompt) - Number(DadosDePromptSegundoValor);
-//   alert(result);
-// }
-
-// par
-
-// if (DadosDePrompt && DadosDePromptSegundoValor % 2 === 0) {
-//   alert("par");
-// } else {
-//   alert("impar");
-// }
+if (DadosDePrompt && DadosDePromptSegundoValor % 2 === 0) {
+  alert("par");
+} else {
+  alert("impar");
+}
