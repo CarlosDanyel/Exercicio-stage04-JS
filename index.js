5c234bef3b5036be497f647aec1d4de317332921
Carlos Danyel <carlosdanyelsilva27@gmail.com>
2024-06-26 15:21:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const DadosDePrompt = prompt("Insira o Primeiro Valor a ser somado");
+const DadosDePromptSegundoValor = prompt("Insira o Segundo Valor");
+
+// Soma
+// const result = Number(DadosDePrompt) + Number(DadosDePromptSegundoValor);
+// alert(result);
+
+// Subtração
+// const result = Number(DadosDePrompt) - Number(DadosDePromptSegundoValor);
+// alert(result);
+
+// Identicos
+// if (DadosDePrompt === DadosDePromptSegundoValor) {
+//   alert("Numeros Identicos");
+// } else {
+//   const result = Number(DadosDePrompt) - Number(DadosDePromptSegundoValor);
+//   alert(result);
+// }
+
+// par
+
+// if (DadosDePrompt && DadosDePromptSegundoValor % 2 === 0) {
+//   alert("par");
+// } else {
+//   alert("impar");
+// }
